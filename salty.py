1dfe175b0b40e51ab28100882275592378a8137a
Jon Horton <jhorton2@gaig.com>
2020-11-09 22:18:58 +0000

    About complex lists

---
+++ salty.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+icecream = ["flavors", "salty"]
+
+icecream.append (99)
+
+print("What is the username? ")
+usr = input()
+
+print(f"{icecream[2]} {icecream[0]}, and {usr} chooses to be {icecream[1]}.")
+
+
