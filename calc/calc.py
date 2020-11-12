ed6220046632fd91f237c2bd6be46aee46ee5dab
Jon Horton <jrhorton@twc.com>
2020-11-11 20:39:43 -0500

    finished calc

--- calc/calc.py
+++ calc/calc.py
@@ -1,30 +1,70 @@
 #!/usr/bin/env python3
-def calc(num1,num2,func):
-    print(num1+func+num2)
 
-while True:
-    try:
-        x1 = float(input("What is the first number?: "))
-        break
-    except:
-        print("We need a number!")
+def add(num1,num2):
+    return(num1+num2)
 
-while True:
-    try:
-        x2 = float(input("What is the second number?: "))
-        break
-    except:
-        print("We need a number!")
+def sub(num1,num2):
+    return(num1-num2)
 
-while True:
-    try:
-        func = input("What do you want to do? +, -, *, / : ") in ["+","-","*","/"]
-        break
-    except:
-        print("We need one of these....  +, -, *, / ")
+def mult(num1,num2):
+    return(num1*num2)
 
+def div(num1,num2):
+    return(num1/num2)
 
-calc(x1,x2,func)
+list1 = ["+","-","*","/"]
+
+esc = ""
+
+while esc != "Q":
+    oper = ""
+
+    while True:
+        try:
+            x1 = float(input("What is the first number?: "))
+            break
+        except:
+            print("We need a number!")
+
+    while oper not in list1:
+        oper = input("What do you want to do? +, -, *, / : ")
+        if oper not in list1:
+            print("We need one of these....  +, -, *, / ")
+
+    while True:
+        try:
+            x2 = float(input("What is the second number?: "))
+            break
+        except:
+            print("We need a number!")
+
+    while oper == "/" and x2 == 0:
+        while True:
+            try:
+                x2 = float(input("You should know better than to try to divide by 0! Enter a new number, you potato: "))
+                break
+            except:
+                print("We need a number!")
+            break
+
+    if oper == "+":
+        res = (add(x1,x2))
+    elif oper == "-":
+        res = (sub(x1,x2))
+    elif oper =="*":
+        res = (mult(x1,x2))
+    elif oper == "/":
+        res = (div(x1,x2))
+
+    if res.is_integer():
+        print(int(res))
+    else:
+       print(res)
+
+    esc = input("Enter Q to quit, or enter any key to continue ").upper()
+
+print("Goodbye")
+
 
 
 
